77be1e7d986bae1b0eecd243803106a0ddb99a39
WladiCosta <wladimircosta.dev@gmail.com>
2022-11-21 00:03:22 -0300

    finished website Pearson Hardman

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,13 +3,63 @@ const menuBtn = document.querySelector("#menu");
 const closeMenuBtn = document.querySelector("#close-menu");
 const menu = document.querySelector("#mobile-navbar");
 
+const desktopLinks = document.querySelectorAll("#navbar a")
+const moblieLinks = document.querySelectorAll("#mobile-navbar a")
+const allLinks = [...desktopLinks, ...moblieLinks]
+
+const slides = document.querySelectorAll(".banner")
+const dots = document.querySelectorAll(".dot")
+
+let slideIndex = 0;
+
 // Funções
+function smoothScroll (e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+    scroll({
+        top: offsetTop,
+        behavior: "smooth",
+    });
+
+    setTimeout(() => {
+        if(menu.classList.contains("menu-active")){
+            menu.classList.remove("menu-active")
+        }
+        
+    }, 500);
+};
+
+
+function showSlides() {
+    for(let i = 0; i < slides.length; i++){
+        slides[i].classList.remove("active");
+        dots[i].classList.remove("active");
+    }
+
+    slideIndex++
+
+    if(slideIndex > slides.length){
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].classList.add("active")
+    dots[slideIndex - 1].classList.add("active")
+
+    setTimeout(showSlides, 3000);
+};
 
 // Eventos
-menuBtn.addEventListener("click", (e) => {
-    menu.classList.add("menu-active")
+[menuBtn, closeMenuBtn].forEach((btn) => {
+    btn.addEventListener("click", (e) =>{
+        menu.classList.toggle("menu-active")
+    });
 });
 
-closeMenuBtn.addEventListener("click", (e) => {
-    menu.classList.remove("menu-active")
+allLinks.forEach((link) => {
+    link.addEventListener("click", smoothScroll);
 });
+
+//Inicialização
+
+showSlides();
